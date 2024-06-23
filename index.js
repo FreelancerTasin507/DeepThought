e6726e715084f9f7c8f5b21cbae060b39d283426
FreelancerTasin507 <freelancertasin507@gmail.com>
2024-06-23 22:38:32 +0600

    drawer section added with mobile responsive

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
     navLink.classList.toggle("show");
   });
 });
-// hambargur 
+// hambargur
+
+// drawer
+document.addEventListener("DOMContentLoaded", function () {
+  const leftDrawer = document.getElementById("leftDrawer");
+  const rightDrawer = document.getElementById("rightDrawer");
+  const leftDrawerDiv = document.querySelector(".left_drawer_div");
+  const rightDrawerDiv = document.querySelector(".right_drawer_div");
+
+  leftDrawerDiv.addEventListener("click", function () {
+    leftDrawer.classList.toggle("expanded");
+  });
+
+  rightDrawerDiv.addEventListener("click", function () {
+    rightDrawer.classList.toggle("expanded");
+  });
+});
+// drawer
